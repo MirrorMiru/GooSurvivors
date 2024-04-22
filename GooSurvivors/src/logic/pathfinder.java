bff46c4a88c38ac0ae54a4ef2f6df1718435a0f7
1923913 <1923913@01-19955.pusd.dom>
2024-04-22 11:47:05 -0700

    adjustments to the Frame.java-deleting unnecessary code.

---
+++ GooSurvivors/src/logic/pathfinder.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class pathfinder {
+
+	public pathfinder() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

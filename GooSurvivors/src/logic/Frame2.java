9b75319e618e58871240c8bf98b09239aab1ccf6
1941422 <1941422@01-19834.pusd.dom>
2024-05-16 11:06:52 -0700

    05/15-my work from this day. it is pushed today due to a technical delay.

--- GooSurvivors/src/logic/Frame2.java
+++ GooSurvivors/src/logic/Frame2.java
@@ -8,25 +8,43 @@ public class Frame2 {
 	ArrayList<Enemy> shootSkells = new ArrayList<Enemy>();
 	ArrayList<ProjectileBullet> bullets = new ArrayList<ProjectileBullet>();
 	
-	private void shoot(ArrayList<Enemy> something, int dmg, int width, int height) {
+	private void shoot(ArrayList<Enemy> something) { //really want to take out the 
 		for( Enemy ss : something) {
-			if( ss.getShootingTimer() == 180) {//need to add an if statement for each subclass shooting enemy or 
-				//make your own shooting class for each subclass
-				ProjectileBullet b = new ProjectileBullet( dmg, width, height, -globalX-380, -globalY + 250, 1);
+			if( ss instanceof ShootingSkeleton ) {
+				if( ((ShootingSkeleton) ss).getShootingTimer() == 40) {
+				ProjectileBullet b = new ProjectileBullet( 10, 20, 20, ((ShootingSkeleton) ss).getX(), ((ShootingSkeleton) ss).getY(), 50);
 				bullets.add( b );
-				if(ss.getX() < -globalX+300) {
+				if( ((ShootingSkeleton) ss).getX() < -globalX+300) {
 					b.setVx(b.getSpeed());
+					System.out.print("SEX IT WORKS");
 				}else {
 					b.setVx(-1*b.getSpeed());
+					System.out.print("SEX IT WORKS");
 				}
-				if(ss.getY() < -globalY+300) {
+				if(((ShootingSkeleton) ss).getY() < -globalY+300) {
 					b.setVy(b.getSpeed());
+					System.out.print("SEX IT WORKS");
 				}else {
 					b.setVy(-1*b.getSpeed());
-			}
+					System.out.print("SEX IT WORKS");
+				}
 			}
 		}//this is to be put in frame
-		//this means that every single enemy with projectiles needs a shootingTimer variable and getter
+		
+		}
 	}
-}
- 
+//in paint method
+	enemyLogic(shootSkells, g);
+	shoot(shootSkells);
+	for( ProjectileBullet bull: bullets) {
+		bull.paint(g);
+	}
+// in spawn enemies method
+	else if(who.toLowerCase().equals("shooting skeletons")) {
+		enemies.add(new ShootingSkeleton());
+	}
+//in start waves method
+	if(waveTimer == 30) {
+		spawnEnemies( 2, shootSkells, "shooting skeletons");
+	}
+} 

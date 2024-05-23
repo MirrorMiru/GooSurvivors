04cf54f2cad44b30f894cd0197101b0ea600f429
1923913 <1923913@01-19955.pusd.dom>
2024-05-23 11:55:00 -0700

    05/23-adjustments to the pathfinding.

--- GooSurvivors/src/logic/pathfinding.java
+++ GooSurvivors/src/logic/pathfinding.java
@@ -15,20 +15,21 @@ import java.util.logging.Level;
 
 public class pathfinding {
 	private ArrayList<Tile> enemy;
-	private ArrayList<Position> map;
-	private Queue<Position> sol;
+	public ArrayList<Position> maze;
+	public Queue<Position> sol;
 	private boolean[][] board;
-	private String[][] xBoard;
+	private String[][] xBoard = new String[27][27];
 	private int startX;
 	private int startY;
 	private char[] types = {0, 1, 2, 3};
 	public int rows = 27;
 	public int cols = 27;
-	Position goal = new Position(13,13, "4");
+	public String filename;
+	public Position goal = new Position(13,13, "4");
 	private int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 	
 	
-	public pathfinding(String s) throws Exception {
+	public pathfinding() {
 		// TODO Auto-generated constructor stub
 		enemy = new ArrayList<Tile>();
 		board = new boolean[27][27];
@@ -62,26 +63,11 @@ public class pathfinding {
 				board[startX][startY] = true;
 			}
 		}
-		String filename = s;
-    	ClassLoader classLoader = Frame.class.getClassLoader();
-		InputStream inputStream = ClassLoader.getSystemResourceAsStream(filename);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-		System.out.println(inputStream != null);
-		String[] dimensions = reader.readLine().split(" ");
-		map = loadMaze(reader,rows,cols);
-		System.out.print(map.size());
 	}
 	public void addTile(Tile e) {
 		enemy.add(e);
 	}
-	public void finder() throws Exception {
-		Scanner userInput = new Scanner(System.in);
-		String c = userInput.nextLine().toLowerCase();
-		pathfinding p = new pathfinding(c);
-		sol = p.findPath(goal, null);
-		
-		
-	}
+	
 	public Queue<Position> findPath(Position start, Player p) {
 		Queue<Position> queue = new LinkedList<>();
 		queue.offer(start);
@@ -103,9 +89,9 @@ public class pathfinding {
             	int newY = current.col + dir[1];
             	
             	if (isValid(newX, newY, goal.row, goal.col)) {
-                    Position n = map.get(newX * cols + newY);
+                    Position n = maze.get(newX * cols + newY);
                     Position neighbor = new Position(n.row, n.col, n.type);
-                    if (neighbor != null && neighbor.row == goal.row && neighbor.col == goal.col && current.type.equals("1") && current.type.equals("3") && current.type.equals("2")) {
+                    if (neighbor != null && neighbor.row == goal.row && neighbor.col == goal.col && current.type.equals("0")) {
                         if (!parentMap.containsKey(neighbor)) {
                         	queue.offer(neighbor);
                             parentMap.put(neighbor, current);
@@ -117,38 +103,41 @@ public class pathfinding {
 		return queue;
 	}
 	private Queue<Position> reconstructPath(Map<Position, Position> parentMap, Position Position) {
-        Queue<Position> path = new LinkedList<>();//create a new queue to return the information
         Position current = goal;
         while (current != null) {//add to the new queue what was in the list
-            path.offer(current);
+            sol.offer(current);
+            maze.add(current);
             current = parentMap.get(current);
         }
-        return path;
+        return sol;
     }
     private boolean isValid(int row, int col, int numRows, int numCols) {
         return row >= 0 && row <= numRows && col >= 0 && col <= numCols;
     }
 
-    public ArrayList<Position> loadMaze(BufferedReader reader, int rows, int cols) throws Exception {
-    	map = new ArrayList<Position>();
+    public ArrayList<Position> loadMaze(Scanner reader, int rows, int cols) throws Exception {
+    	maze = new ArrayList<Position>();
+    	int j = 0;
     	for (int i = 0; i < rows; i++) {
-			String line = reader.readLine();
-			for (int j = 0; j < cols; j++) {
+			String line = reader.toString();
+			while (line.length() > 0) {
 				String c = line.substring(i,i+1);
 				if (!c.equals(",")) {
 					Position p = new Position(i,j,c);
-					map.add(p);
+					maze.add(p);
 					xBoard[i][j] = c;
 				}
+				line = line.substring(i+1);
 			}
+			j++;
 		}
 		System.out.println("all positions loaded");
-		return map;
+		return maze;
 	}
     
     public void paint(Graphics g) {
-    	for (int i = 0;  i < map.size(); i++) {
-    		Tile t = new Tile(map.get(i).row, map.get(i).col, Integer.parseInt(map.get(i).type));
+    	for (int i = 0;  i < sol.size(); i++) {
+    		Tile t = new Tile(maze.get(i).row, maze.get(i).col, Integer.parseInt(maze.get(i).type));
     		t.paint(g);
     	}
     }  

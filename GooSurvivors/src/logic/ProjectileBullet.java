6b855d24152bc6b4f5e7e80d9ad9feefa0b0c865
1941422 <1941422@01-19834.pusd.dom>
2024-05-23 12:08:37 -0700

    updated ProjectileBullet

--- GooSurvivors/src/logic/ProjectileBullet.java
+++ GooSurvivors/src/logic/ProjectileBullet.java
@@ -93,7 +93,7 @@ public class ProjectileBullet{
 		
 		//if scaling images with scale var, make sure w/h reflect what we see on screen
 		Rectangle otherObj = new Rectangle(x,y,width,height);
-		Rectangle thisObj = new Rectangle(this.x+55,this.y+55, getWidth(),getHeight());
+		Rectangle thisObj = new Rectangle(this.x,this.y, getWidth(),getHeight());
 
 		return thisObj.intersects(otherObj);
 	}

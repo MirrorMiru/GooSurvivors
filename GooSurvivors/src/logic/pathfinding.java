359941d5fe76c6ff32f73af5260ae335527b9a2f
1923913 <1923913@01-19955.pusd.dom>
2024-05-24 10:38:08 -0700

    05/24-adjustments for pathfinding.

--- GooSurvivors/src/logic/pathfinding.java
+++ GooSurvivors/src/logic/pathfinding.java
@@ -2,6 +2,7 @@ package logic;
 
 import java.awt.Graphics;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -14,24 +15,23 @@ import java.util.Scanner;
 import java.util.logging.Level;
 
 public class pathfinding {
-	private ArrayList<Tile> enemy;
+	private ArrayList<Position> entry;
 	public ArrayList<Position> maze;
-	public Queue<Position> sol;
+	public ArrayList<Position> sol = new ArrayList<Position>();
 	private boolean[][] board;
 	private String[][] xBoard = new String[27][27];
 	private int startX;
 	private int startY;
-	private char[] types = {0, 1, 2, 3};
 	public int rows = 27;
 	public int cols = 27;
 	public String filename;
 	public Position goal = new Position(13,13, "4");
+	public Position start;
 	private int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 	
 	
 	public pathfinding() {
 		// TODO Auto-generated constructor stub
-		enemy = new ArrayList<Tile>();
 		board = new boolean[27][27];
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0;  j < board[0].length; j++) {
@@ -44,11 +44,13 @@ public class pathfinding {
 				startX = 0;
 				startY = (int)((Math.random()*27));
 				board[startX][startY] = true;
+				start = new Position(startX, startY, "4");
 			}
 			else {
 				startX = (int)((Math.random()*27));
 				startY = 0;
 				board[startX][startY] = true;
+				start = new Position(startX, startY, "4");
 			}
 		}
 		else {
@@ -56,19 +58,18 @@ public class pathfinding {
 				startX = board.length-1;
 				startY = (int)((Math.random()*27));
 				board[startX][startY] = true;
+				start = new Position(startX, startY, "4");
 			}
 			else {
 				startX = (int)((Math.random()*27));
 				startY = board[0].length-1;
 				board[startX][startY] = true;
+				start = new Position(startX, startY, "4");
 			}
 		}
 	}
-	public void addTile(Tile e) {
-		enemy.add(e);
-	}
 	
-	public Queue<Position> findPath(Position start, Player p) {
+	public ArrayList<Position> findPath(ArrayList<Position> maze, Position start, Position goal) {
 		Queue<Position> queue = new LinkedList<>();
 		queue.offer(start);
 
@@ -81,6 +82,7 @@ public class pathfinding {
             
             if (current.row == goal.row && current.col == goal.col) {
                 // If the goal is reached, reconstruct and return the path
+            	System.out.println("it works");
                 return reconstructPath(parentMap, goal);
             }
             
@@ -89,8 +91,7 @@ public class pathfinding {
             	int newY = current.col + dir[1];
             	
             	if (isValid(newX, newY, goal.row, goal.col)) {
-                    Position n = maze.get(newX * cols + newY);
-                    Position neighbor = new Position(n.row, n.col, n.type);
+                    Position neighbor = maze.get(newX * cols + newY);
                     if (neighbor != null && neighbor.row == goal.row && neighbor.col == goal.col && current.type.equals("0")) {
                         if (!parentMap.containsKey(neighbor)) {
                         	queue.offer(neighbor);
@@ -100,53 +101,54 @@ public class pathfinding {
                 }
             }
         }
-		return queue;
+        System.out.println("no work");
+		return null;
 	}
-	private Queue<Position> reconstructPath(Map<Position, Position> parentMap, Position Position) {
+	private ArrayList<Position> reconstructPath(Map<Position, Position> parentMap, Position Position) {
         Position current = goal;
         while (current != null) {//add to the new queue what was in the list
-            sol.offer(current);
-            maze.add(current);
+        	sol.add(0,current);
             current = parentMap.get(current);
         }
+        System.out.println(sol);
         return sol;
     }
     private boolean isValid(int row, int col, int numRows, int numCols) {
         return row >= 0 && row <= numRows && col >= 0 && col <= numCols;
     }
 
-    public ArrayList<Position> loadMaze(Scanner reader, int rows, int cols) throws Exception {
+    public ArrayList<Position> loadMaze(String name, int rows, int cols) throws Exception {
     	maze = new ArrayList<Position>();
     	int j = 0;
-    	for (int i = 0; i < rows; i++) {
-			String line = reader.toString();
-			while (line.length() > 0) {
-				String c = line.substring(i,i+1);
-				if (!c.equals(",")) {
-					Position p = new Position(i,j,c);
-					maze.add(p);
-					xBoard[i][j] = c;
+    	try {
+			File f = new File(name);
+			Scanner s = new Scanner(f);
+			while (s.hasNext()) {
+				String[] row = s.nextLine().split(",");
+				for(int i = 0; i < row.length; i++) {
+					maze.add(new Position(j,i,row[i].substring(0, 1)));
+					xBoard[j][i] = row[i].substring(0, 1);
 				}
-				line = line.substring(i+1);
+				j++;
+			
 			}
-			j++;
+			
+			
+			s.close();
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		System.out.println("all positions loaded");
 		return maze;
 	}
     
     public void paint(Graphics g) {
-    	for (int i = 0;  i < sol.size(); i++) {
-    		Tile t = new Tile(maze.get(i).row, maze.get(i).col, Integer.parseInt(maze.get(i).type));
-    		t.paint(g);
-    	}
+    	
     }  
     public int[][] returnBoard() {
     	int[][] board = new int[27][27];
     	int s = sol.size();
     	for (int i = 0; i < s; i++) {
-    		board[sol.peek().row][sol.peek().col] = 1;
-    		sol.remove();
+    		board[sol.get(i).row][sol.get(i).col] = 1;
     	}
     	return board;
     }
